f02c97fb88ce4ecf1d2fc9b62529e3782dcaa63c
sonan1399 <sonan1399@gmail.com>
2024-02-28 10:03:49 +0530

    First commit of Selenium code

---
+++ SelenIntro.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SelenIntro {
+
+	public static void main(String[] args) {
+		
+		//Invoking Browser
+//		System.setProperty("webdriver.com.driver","C:/Users/snthadev/Documents/chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+		
+		System.setProperty("webdriver.com.driver","C:/Users/snthadev/Documents/msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		
+//		WebDriver driver = new FirefoxDriver();
+//		System.setProperty("webdriver.com.driver","C:/Users/snthadev/Documents/geckodriver.exe");
+		
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+		driver.quit();
+		
+	}
+
+}
